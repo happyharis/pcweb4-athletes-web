cf10e7bfd3c64fc3a2dda4e19c6de45c7463ae02
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-13 19:49:30 +0800

    Add express js

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 const port = 8080;
 app.set("view engine", "ejs");
 
+const players = [
+  {
+    name: "Haris weelel",
+    description: "He really handsome",
+    image: "https://picsum.photos/400",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.render(index.ejs);
+  res.render("index.ejs", {
+    players,
+  });
 });
 
 app.listen(port, () => {
