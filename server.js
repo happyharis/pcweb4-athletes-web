6920459e94f18e43285b8c57bac6a28e5f9849b4
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-13 20:15:55 +0800

    Add add page

--- server.js
+++ server.js
@@ -13,10 +13,34 @@ const pool = mysql
 const app = express();
 const port = 8080;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 async function getPlayers() {
   const [rows] = await pool.query("SELECT * FROM players;");
   return rows;
+}
+
+async function getPlayer(id) {
+  const [rows] = await pool.query(
+    `
+      select *
+      from players
+      where id = ?
+      `,
+    [id]
+  );
+  return rows;
+}
+
+async function addPlayer(name, description, image) {
+  const [result] = await pool.query(
+    `INSERT INTO players (name, description, image)
+      VALUES (?, ?, ?)
+      `,
+    [name, description, image]
+  );
+  const id = result.insertId;
+  return getPlayer(id);
 }
 
 app.get("/", async (req, res) => {
@@ -24,6 +48,15 @@ app.get("/", async (req, res) => {
   res.render("index.ejs", {
     players,
   });
+});
+app.get("/add", async (req, res) => {
+  res.render("add.ejs");
+});
+app.post("/add", async (req, res) => {
+  const { name, description, image } = req.body;
+  const note = await addPlayer(name, description, image);
+  //   res.status(201).send(note);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
