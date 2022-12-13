8194d0040775dc77075b269989d09692ec5dca7b
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-13 19:57:00 +0800

    Add mysql2

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express = require("express");
+import mysql from "mysql2";
+import express from "express";
+
+const pool = mysql
+  .createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "athletes",
+  })
+  .promise();
+
 const app = express();
 const port = 8080;
 app.set("view engine", "ejs");
 
-const players = [
-  {
-    name: "Haris weelel",
-    description: "He really handsome",
-    image: "https://picsum.photos/400",
-  },
-];
+async function getPlayers() {
+  const [rows] = await pool.query("SELECT * FROM players;");
+  return rows;
+}
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const players = await getPlayers();
   res.render("index.ejs", {
     players,
   });
